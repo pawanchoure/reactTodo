f5ffaeff1a97e2aa5ac66e70e9fde0b611ebe421
Pawan Choure <pawanchoure@gmail.com>
2021-06-01 14:09:30 +0530

    Component Breakup

--- src/App.js
+++ src/App.js
@@ -1,81 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TodoBanner } from "./TodoBanner";
+import { TodoCreator } from "./TodoCreator";
+import { TodoRow } from "./TodoRow";
+import { VisibilityControl } from "./VisibilityControl";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        userName: "Pawan",
-        todoItems: [{ action: "Buy Flowers", done: false },
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: "Adam",
+            todoItems: [{ action: "Buy Flowers", done: false },
                         { action: "Get Shoes", done: false },
                         { action: "Collect Tickets", done: true },
-                        { action: "Call Joe", done: true }],
-            newItemText: ""
+                        { action: "Call Joe", done: false }],
+            showCompleted: true
+        }
     }
-}
 
-updateNewTextValue = (event) => {
-  this.setState({ newItemText: event.target.value });
-}
+    updateNewTextValue = (event) => {
+        this.setState({ newItemText: event.target.value });
+    }
 
-createNewTodo = () => {
-  if (!this.state.todoItems
-           .find(item => item.action === this.state.newItemText)) {
-      this.setState({
-          todoItems: [...this.state.todoItems,
-              { action: this.state.newItemText, done: false }],
-          newItemText: ""
-      });
-  }
-}
+    createNewTodo = (task) => {
+        if (!this.state.todoItems.find(item => item.action === task)) {
+            this.setState({ 
+                todoItems: [...this.state.todoItems, { action: task, done: false }]
+            }, () => localStorage.setItem("todos", JSON.stringify(this.state)));        
+        }
+    }
 
-toggleTodo = (todo) => this.setState({ todoItems:
-  this.state.todoItems.map(item => item.action === todo.action
-      ? { ...item, done: !item.done } : item) });
-todoTableRows = () => this.state.todoItems.map(item =>
-  <tr key={ item.action }>
-      <td>{ item.action}</td>
-      <td>
-          <input type="checkbox" checked={ item.done }
-              onChange={ () => this.toggleTodo(item) } />
-      </td>
-  </tr> );
+    toggleTodo = (todo) => this.setState({ todoItems: 
+        this.state.todoItems.map(item => item.action === todo.action 
+            ? { ...item, done: !item.done } : item) });
+    
+    todoTableRows = (doneValue) => this.state.todoItems
+        .filter(item => item.done === doneValue).map(item => 
+            <TodoRow key={ item.action } item={ item } 
+                callback={ this.toggleTodo } />)
+         
 
-changeStateData = () => {
-  this.setState({
-      userName: this.state.userName === "Pawan" ? "Hrishi" : "Ketan"
-  })
-}
-  render() {
-    return (
-      <div>
-        <h4 className="bg-primary text-white text-center p-2">
-        { this.state.userName }'s To Do List
-        ({ this.state.todoItems.filter(t => !t.done).length} items to do)
-        <button className="btn btn-primary m-2"
-                        onClick={ this.changeStateData }>
-                    Change
-        </button>
-                
-            </h4>
+    componentDidMount = () => {
+        let data = localStorage.getItem("todos");
+        this.setState(data != null 
+            ? JSON.parse(data) 
+            :  { 
+                userName: "Adam",
+                todoItems: [{ action: "Buy Flowers", done: false },
+                            { action: "Get Shoes", done: false },
+                            { action: "Collect Tickets", done: true },
+                            { action: "Call Joe", done: false }],
+                showCompleted: true    
+            });
+    }
+
+    render = () => 
+        <div>
+            <TodoBanner name={ this.state.userName } 
+                tasks={this.state.todoItems } />
             <div className="container-fluid">
-                <div className="my-1">
-                    <input className="form-control"
-                        value={ this.state.newItemText }
-                        onChange={ this.updateNewTextValue } />
-                    <button className="btn btn-primary mt-1"
-                        onClick={ this.createNewTodo }>Add</button>
-                </div>
+                <TodoCreator callback={ this.createNewTodo } />
                 <table className="table table-striped table-bordered">
                     <thead>
                         <tr><th>Description</th><th>Done</th></tr>
                     </thead>
-                    <tbody>{ this.todoTableRows() }</tbody>
+                    <tbody>{ this.todoTableRows(false) }</tbody>
                 </table>
-            </div>
-      </div>
-    )
-  }
-}
 
+                <div className="bg-secondary text-white text-center p-2">
+                    <VisibilityControl description="Completed Tasks" 
+                        isChecked={this.state.showCompleted}
+                        callback={ (checked) => 
+                            this.setState({ showCompleted: checked })} />
+                </div>  
+
+                { this.state.showCompleted &&                       
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                            <tr><th>Description</th><th>Done</th></tr>
+                        </thead>
+                        <tbody>{ this.todoTableRows(true) }</tbody>
+                    </table>
+                }
+            </div>
+        </div>
+}
